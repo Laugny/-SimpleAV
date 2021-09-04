5fe278940b20ea79a3ab53a3e17e8f14798f8a21
Laugny <79530050+Laugny@users.noreply.github.com>
2021-09-04 15:13:18 +0200

    Add files via upload

---
+++ Simple AV/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Simple_AV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtMD5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.lblScannedDocs = new System.Windows.Forms.Label();
+            this.lblScanning = new System.Windows.Forms.Label();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toggleAdvancedModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(374, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // browseToolStripMenuItem
+            // 
+            this.browseToolStripMenuItem.Name = "browseToolStripMenuItem";
+            this.browseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.browseToolStripMenuItem.Text = "Browse";
+            this.browseToolStripMenuItem.Click += new System.EventHandler(this.browseToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File:";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(44, 39);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(310, 20);
+            this.txtFile.TabIndex = 3;
+            // 
+            // txtMD5
+            // 
+            this.txtMD5.Location = new System.Drawing.Point(44, 65);
+            this.txtMD5.Name = "txtMD5";
+            this.txtMD5.ReadOnly = true;
+            this.txtMD5.Size = new System.Drawing.Size(310, 20);
+            this.txtMD5.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MD5:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(41, 88);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(63, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status: N/A";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(279, 91);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 7;
+            this.btnScan.Text = "scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // lblScannedDocs
+            // 
+            this.lblScannedDocs.AutoSize = true;
+            this.lblScannedDocs.Location = new System.Drawing.Point(41, 105);
+            this.lblScannedDocs.Name = "lblScannedDocs";
+            this.lblScannedDocs.Size = new System.Drawing.Size(81, 13);
+            this.lblScannedDocs.TabIndex = 8;
+            this.lblScannedDocs.Text = "Scanned Docs:";
+            this.lblScannedDocs.Visible = false;
+            // 
+            // lblScanning
+            // 
+            this.lblScanning.AutoSize = true;
+            this.lblScanning.Location = new System.Drawing.Point(41, 122);
+            this.lblScanning.Name = "lblScanning";
+            this.lblScanning.Size = new System.Drawing.Size(58, 13);
+            this.lblScanning.TabIndex = 9;
+            this.lblScanning.Text = "Scanning: ";
+            this.lblScanning.Visible = false;
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toggleAdvancedModeToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // toggleAdvancedModeToolStripMenuItem
+            // 
+            this.toggleAdvancedModeToolStripMenuItem.Name = "toggleAdvancedModeToolStripMenuItem";
+            this.toggleAdvancedModeToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.toggleAdvancedModeToolStripMenuItem.Text = "Toggle Advanced Mode";
+            this.toggleAdvancedModeToolStripMenuItem.Click += new System.EventHandler(this.toggleAdvancedModeToolStripMenuItem_Click_1);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.clearToolStripMenuItem.Text = "Clear all";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 141);
+            this.Controls.Add(this.lblScanning);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtMD5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.lblScannedDocs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple AV";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem browseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtMD5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Label lblScannedDocs;
+        private System.Windows.Forms.Label lblScanning;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toggleAdvancedModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Simple AV/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+
+namespace Simple_AV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetMD5FromFile(string filenPath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filenPath))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty).ToLower();
+                }
+            }
+        }
+
+        private void browseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "select a File, you think could be a virus";
+            dlg.Filter = "ALL FILES | *.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = dlg.FileName;
+                txtMD5.Text = GetMD5FromFile(dlg.FileName);
+            }
+            lblStatus.Text = "Status: N/A";
+            lblScannedDocs.Text = "Scanned Docs:";
+            lblScanning.Text = "Scanning:";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(() => {
+
+            int num = 0;
+
+            if (txtFile.Text != "")
+            {
+                again:
+                    lblScannedDocs.Invoke(new Action(() => {
+                        lblScannedDocs.Text = $"Scanned Docs: {num}";
+                    }));
+
+                int rest = 5 - num.ToString().Length;
+
+                WebClient wc = new WebClient();
+
+                switch (rest)
+                {
+                    case 4:
+                        String md5Sig4 = wc.DownloadString("https://virusshare.com/hashfiles/VirusShare_" + "0000" + num.ToString() + ".md5");
+                          
+                        if (md5Sig4.Contains(txtMD5.Text))
+                        {
+                                lblStatus.Invoke(new Action(() => {
+                                    lblStatus.Text = "Infected";
+                                    lblStatus.ForeColor = Color.Red;
+                                }));
+                        }
+                        else
+                        {
+                            num++;
+                                lblScanning.Invoke(new Action(() => { lblScanning.Text = "Scanning: https://virusshare.com/hashfiles/VirusShare_" + "0000" + num.ToString() + ".md5"; }));
+                                goto again;
+                        }
+                        break;
+
+                    case 3:
+                            String md5Sig3 = wc.DownloadString("https://virusshare.com/hashfiles/VirusShare_" + "000" + num.ToString() + ".md5");
+                        
+                            if (md5Sig3.Contains(txtMD5.Text))
+                            {
+                                lblStatus.Invoke(new Action(() => {
+                                    lblStatus.Text = "Infected";
+                                    lblStatus.ForeColor = Color.Red;
+                                }));
+                            }
+                            else
+                            {
+                                num++;
+                                lblScanning.Invoke(new Action(() => { lblScanning.Text = "Scanning: https://virusshare.com/hashfiles/VirusShare_" + "000" + num.ToString() + ".md5"; }));
+                                goto again;
+                            } 
+                            break;
+
+                    case 2:
+                            String md5Sig2 = wc.DownloadString("https://virusshare.com/hashfiles/VirusShare_" + "00" + num.ToString() + ".md5");
+                            
+                            if (md5Sig2.Contains(txtMD5.Text))
+                            {
+                                lblStatus.Invoke(new Action(() => {
+                                    lblStatus.Text = "Infected";
+                                    lblStatus.ForeColor = Color.Red;
+                                }));
+                            }
+                            else
+                            {
+                                num++;
+                                lblScanning.Invoke(new Action(() => { lblScanning.Text = "Scanning: https://virusshare.com/hashfiles/VirusShare_" + "00" + num.ToString() + ".md5"; }));
+                                if (num == 389)
+                                {
+                                    lblStatus.Invoke(new Action(() => {
+                                        lblStatus.Text = "Clean";
+                                        lblStatus.ForeColor = Color.Green;
+                                    }));
+                                }
+                                else
+                                {
+                                    goto again;
+                                }
+                            }
+                            break;                  
+                }               
+            }
+            else
+            {
+                MessageBox.Show("Please select a File!","Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            });
+            th.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void toggleAdvancedModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool advanced = false;
+            if (advanced)
+            {
+                advanced = false;
+                lblScannedDocs.Visible = false;
+                lblScanning.Visible = false;
+            }
+            else if (!advanced)
+            {
+                advanced = true;
+                lblScannedDocs.Visible = true;
+                lblScanning.Visible = true;
+            }
+        }
+
+        bool advanced = false;
+        private void toggleAdvancedModeToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (advanced)
+            {
+                advanced = false;
+                lblScannedDocs.Visible = false;
+                lblScanning.Visible = false;
+            }
+            else if (!advanced)
+            {
+                advanced = true;
+                lblScannedDocs.Visible = true;
+                lblScanning.Visible = true;
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Status: N/A";
+            lblStatus.ForeColor = Color.Black;
+
+            lblScannedDocs.Text = "Scanned Docs:";
+            lblScanning.Text = "Scanning:";
+
+            txtFile.Text = "";
+            txtMD5.Text = "";
+        }
+    }
+}
